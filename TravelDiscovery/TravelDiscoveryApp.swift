8f2e7afc2499b9657e99df151d4b70e5cc359930
Vladimir Tambovtsev <tambovcev99@mail.ru>
2021-02-06 03:31:06 +0300

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Владимир on 06.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
